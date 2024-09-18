7e7655cd61886b746933aaed86debdc56164d7db
Shadman Rahman <shadmxn@gmail.com>
2024-09-18 19:06:01 -0400

    implement topics at the top of the page by grouping TopicListItem components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PhotoList from './components/PhotoList';
+import TopicList from './components/TopicList'; // Import TopicList
 import './App.scss';
 
 const App = () => (
   <div className="App">
+    <TopicList />  {/* Add TopicList */}
     <PhotoList />
   </div>
 );
