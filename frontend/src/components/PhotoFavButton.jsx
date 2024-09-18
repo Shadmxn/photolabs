2ab3fb647f75a7b1338b04e0d457cd9e8f8d1d10
Shadman Rahman <shadmxn@gmail.com>
2024-09-18 16:20:39 -0400

    implement add the favorite icon activity

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,14 +1,20 @@
-import React, { useCallback, useState } from 'react';
-
-import FavIcon from './FavIcon';
+import React, { useState } from 'react';
+import FavIcon from './FavIcon';  // Import FavIcon
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  // State to track whether the photo is favorited
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  // Function to toggle the favorite state
+  const toggleFavorite = () => {
+    setIsFavorited(prev => !prev);
+  };
+
   return (
-    <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
-      </div>
+    <div className="photo-list__fav-icon" onClick={toggleFavorite}>
+      {/* Pass the isFavorited state as 'selected' to FavIcon */}
+      <FavIcon selected={isFavorited} displayAlert={false} />
     </div>
   );
 }
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from './PhotoFavButton';  // Import PhotoFavButton
 
 const PhotoListItem = ({ photo }) => {
-  const { id, location, imageSource, username, profile } = photo;
-  
+  const { location, imageSource, username, profile } = photo;
+
   return (
     <div className="photo-list__item">
-      <img src={profile} alt={`${username}'s profile`} />
-      <h3>{username}</h3>
-      <img src={imageSource} alt={`From ${location.city}, ${location.country}`} />
-      <p>{location.city}, {location.country}</p>
+      <div className="photo-list__user-details">
+        <img src={profile} alt={`${username}'s profile`} className="photo-list__user-profile" />
+        <h3 className="photo-list__user-info">{username}</h3>
+      </div>
+
+      <img src={imageSource} alt={`From ${location.city}, ${location.country}`} className="photo-list__image" />
+
+      <p className="photo-list__user-location">{location.city}, {location.country}</p>
+
+      {/* Add the PhotoFavButton to the right side */}
+      <div className="photo-list__fav-container">
+        <PhotoFavButton />
+      </div>
     </div>
   );
 };
 
 PhotoListItem.propTypes = {
   photo: PropTypes.shape({
-    id: PropTypes.string.isRequired,
     location: PropTypes.shape({
       city: PropTypes.string.isRequired,
       country: PropTypes.string.isRequired,
