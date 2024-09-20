c39fddaebf3bb6d55ae946b5cc66d04e49577064
Shadman Rahman <shadmxn@gmail.com>
2024-09-19 20:52:07 -0400

    implement final additions to connect the favicon and modal so it works with the notification and is seamless

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,11 +1,13 @@
-// PhotoDetailsModal.jsx
 import React from 'react';
 import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 import PhotoList from '../components/PhotoList';
+import PhotoFavButton from '../components/PhotoFavButton';
 
 const PhotoDetailsModal = ({ photo, similarPhotos, closeModal, favoritePhotos, toggleFavorite }) => {
   if (!photo) return null;
+
+  const isFavorite = favoritePhotos.includes(photo.id);
 
   return (
     <div className="photo-details-modal">
@@ -13,17 +15,20 @@ const PhotoDetailsModal = ({ photo, similarPhotos, closeModal, favoritePhotos, t
         <img src={closeSymbol} alt="close symbol" />
       </button>
 
-      <img src={photo.imageSource} alt="Large version" className="photo-details-modal__image" />
+      <div className="photo-details-modal__image-container">
+        <img src={photo.imageSource} alt="Large version" className="photo-details-modal__image" />
+        <PhotoFavButton 
+          isFavorite={isFavorite} 
+          toggleFavorite={() => toggleFavorite(photo.id)} 
+        />
+      </div>
+
       <p>{photo.location.city}, {photo.location.country}</p>
       <p>Uploaded by: {photo.username}</p>
 
       <div className="photo-details-modal__similar-photos">
         <h3>Similar Photos</h3>
-        <PhotoList 
-          photos={Array.isArray(similarPhotos) ? similarPhotos : []} 
-          favoritePhotos={favoritePhotos} // Pass favoritePhotos
-          toggleFavorite={toggleFavorite}   // Pass toggleFavorite
-        />
+        <PhotoList photos={Array.isArray(similarPhotos) ? similarPhotos : []} />
       </div>
     </div>
   );
