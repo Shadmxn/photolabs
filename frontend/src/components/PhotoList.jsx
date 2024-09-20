09ab0c08772f4b93f5976e48559604def334d0a6
Shadman Rahman <shadmxn@gmail.com>
2024-09-19 20:20:23 -0400

    fix no photos available

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,26 +1,31 @@
+// src/components/PhotoList.jsx
 import React from 'react';
 import PhotoListItem from './PhotoListItem';
 import '../styles/PhotoList.scss';
 
-const PhotoList = ({ photos, toggleFavorite, favoritePhotos, onPhotoClick }) => (
-  <ul className="photo-list">
-    {photos.map(photo => (
-      <li key={photo.id}>
-        <PhotoListItem 
-          photo={{
-            id: photo.id,
-            location: photo.location,
-            imageSource: photo.urls.regular,
-            username: photo.user.username,
-            profile: photo.user.profile,
-            isFavorite: favoritePhotos.includes(photo.id),
-          }} 
-          toggleFavorite={toggleFavorite}
-          onPhotoClick={onPhotoClick}
-        />
-      </li>
-    ))}
-  </ul>
-);
+const PhotoList = ({ photos, toggleFavorite, favoritePhotos, onPhotoClick }) => {
+  if (!photos || photos.length === 0) return <p>No similar photos available.</p>;
+
+  return (
+    <ul className="photo-list">
+      {photos.map(photo => (
+        <li key={photo.id}>
+          <PhotoListItem 
+            photo={{
+              id: photo.id,
+              location: photo.location,
+              imageSource: photo.urls.regular, // Adjusted to use the correct image source
+              username: photo.user.username,
+              profile: photo.user.profile,
+              isFavorite: favoritePhotos.includes(photo.id),
+            }} 
+            toggleFavorite={toggleFavorite}
+            onPhotoClick={onPhotoClick} // Pass onPhotoClick for modal functionality
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default PhotoList;
